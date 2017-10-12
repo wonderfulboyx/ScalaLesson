a589e37122a6462924096b86d85091a194415869
wonderfulboyx <wonderfulboyx@wonderfulboyx.local>
2017-10-12 23:33:34 +0900

    commit

--- redbook/src/main/scala/error.scala
+++ redbook/src/main/scala/error.scala
@@ -56,8 +56,12 @@ object Option {
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
 
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = {
+    a.flatMap( aa => b)
+  }
+
   // a bit later in the chapter we'll learn nicer syntax for
   // writing functions like this
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
-  a flatMap (aa => b map (bb => f(aa, bb)))
+//  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+//  a flatMap (aa => b map (bb => f(aa, bb)))
 }
